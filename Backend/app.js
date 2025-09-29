4ed93b1d36e28475376476a4b9292c207267a43e
Rajnandini Jathar <rajnandinijathar2001@gmail.com>
2025-09-29 13:56:45 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config({ path: "./config/config.env" });
 
 app.use(cors({
- origin: "*",
+ origin: "hotelrsj-cso960thz-rajnandinis-projects-9e0efaf8.vercel.app",
   methods: ["POST"],
   credentials: true,
   
